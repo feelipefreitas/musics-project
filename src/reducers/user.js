ea3e77b8dba2fa21fdb334c9501a8d177f620b2d
Felipe da Silva Freitas <feelipe.freitas97@gmail.com>
2019-01-28 14:41:52 -0200

    Adding SIGN_OUT action

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -9,6 +9,8 @@ export default (state = INITIAL_USER_STATE, action) => {
     switch(action.type) {
         case SIGN_IN:
             return { ...state, ...action.payload };
+        case SIGN_OUT:
+            return { ...state, userId: null, userName: ''};
         default:
             return state;
     }
